b867b4568b034088bfe6e877c1a2bb52ae7419fa
blakeanedved <blakeanedved@gmail.com>
2019-02-11 22:07:57 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	author="Blake Nedved",
 	author_email="blakeanedved@gmail.com",
 	description="A terminal based ruler for displaying the length of a file, primarily used for codegolfing",
-	url="",
+	url="https://github.com/blakeanedved/ascii-ruler",
     entry_points={
         'console_scripts': [
             'ascii-ruler=__init__:main'
